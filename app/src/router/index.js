6cce0581bfcbdd750b628bb84c5f37e43afe1c6a
petergaobro <pgao@isw.net.au>
2023-08-10 13:01:13 +1000

    feat: change website title 'app' to ip address tracker

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -5,13 +5,22 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: "IP Address Tracker"
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// change website title from app to IP Address Tracker
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next()
 })
 
 export default router
